f7afe5519729bdd89f79826c6b411c24cc52c5bd
Ta Jour Harris <tjharris705@yahoo.com>
2021-11-14 19:02:25 -0800

    Added model views to admin and added restrictions to who can visit admin page and view certain views

--- backend.py
+++ backend.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_admin.contrib import sqla
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, ForeignKey
 import os.path
-from flask_admin import Admin
+from flask_admin import Admin, expose, AdminIndexView
 from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user
 from flask_admin.contrib.sqla import ModelView
 
@@ -15,7 +15,6 @@ db = SQLAlchemy(app)
 engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"], echo=True)
 
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-admin = Admin(app, name='teacher-view', template_mode='bootstrap3')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -28,6 +27,27 @@ def load_user(user_id):
     return Users.query.get(user_id)
 
 
+# restrict admin page from being accessed by students
+class AdminIndex(AdminIndexView):
+    @expose('/')
+    def index(self):
+        if not is_teacher(current_user) and not current_user.is_authenticated:
+            return redirect(url_for('login'))
+        return super(AdminIndex, self).index()
+
+    def is_accessible(self):
+        return is_teacher(current_user)
+
+
+class AdminMixin:
+    def is_accessible(self):
+        return current_user.is_authenticated and is_teacher(current_user)
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for('login', next=request.url))
+
+
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     username = db.Column(db.String(25), nullable=False)
@@ -35,9 +55,6 @@ class Users(UserMixin, db.Model):
 
     def check_password(self, password):
         return self.password == password
-
-
-admin.add_view(ModelView(Users, db.session))
 
 
 class Classes(db.Model):
@@ -58,7 +75,7 @@ class Students(db.Model):
     classes = db.relationship('Classes', secondary='enrollment')
 
 
-class Teachers(db.Model):
+class Teachers(AdminMixin, db.Model):
     id = db.Column(db.Integer, nullable=False, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(25), nullable=False)
     last_name = db.Column(db.String(25), nullable=False)
@@ -66,21 +83,35 @@ class Teachers(db.Model):
     classes = db.relationship('Classes', backref=db.backref('classes', lazy=True))
 
 
+# joint table for M:M relationships
 class Enrollment(db.Model):
     class_id = db.Column('class_id', db.Integer, db.ForeignKey('classes.id'), primary_key=True)
     student_id = db.Column('student_id', db.Integer, db.ForeignKey('students.id'), primary_key=True)
     grade = db.Column('grade', db.String(4), nullable=False)
 
 
-class AdminView(sqla.ModelView):
+# The following view classes are views added to admin page
+class UserView(AdminMixin, sqla.ModelView):
+    column_exclude_list = ['Password', ]
+    pass
 
-    def is_accessible(self):
-        return login.current_user.is_authenticated
 
-    def inaccessible_callback(self, name, **kwargs):
-        # redirect to login page if user doesn't have access
-        return redirect(url_for('login', next=request.url))
+class ClassView(AdminMixin, sqla.ModelView):
+    pass
 
+
+class StudentView(AdminMixin, sqla.ModelView):
+    pass
+
+
+# sets up admin page, index_view takes the view perms from AdminIndex()
+admin = Admin(app, name='teacher-view', template_mode='bootstrap3', index_view=AdminIndex())
+
+
+# adding views to flask-admin
+admin.add_view(UserView(Users, db.session))
+admin.add_view(ClassView(Classes, db.session))
+admin.add_view(StudentView(Students, db.session))
 
 db.create_all()
 db.session.commit()
@@ -158,6 +189,15 @@ def get_class_capacity(user):
         return numbers
 
 
+def check_class_capacity(student_class):
+    return student_class.enrolled == student_class.size
+
+
+def add_class(student_id, class_id):
+    db.session.add(Enrollment(student_id=student_id, class_id=class_id))
+    db.session.commit()
+
+
 @app.route('/')
 def home():
     return redirect(url_for('login'))
@@ -170,7 +210,7 @@ def login(error='Invalid username or password'):
 
         # if the user is authenticated and a teacher, load the teacher page. Later to change to /admin
         if is_teacher(current_user):
-            return redirect(url_for('user_page', user_id=current_user.id))
+            return redirect(url_for('teacher_page', user_id=current_user.id))
 
         # base return value for students
         return redirect(url_for('user_page', user_id=current_user.id))
@@ -188,26 +228,67 @@ def login(error='Invalid username or password'):
 
 
 # app route for a logged in user who isn't a teacher
-@app.route('/user/<user_id>/classes', methods=['POST', 'GET'])
+@app.route('/user/classes', methods=['POST', 'GET'])
 @login_required
-def user_page(user_id):
-    user = Users.query.filter_by(id=user_id).first()
-    student = Students.query.filter(Students.user_id == user.id).first()
-    name = student.first_name
+def user_page():
+    if request.method == 'POST':
+        return redirect(url_for('registration'))
+    else:
+        user = Users.query.filter_by(id=current_user.id).first()
+        student = Students.query.filter(Students.user_id == user.id).first()
+        name = student.first_name
+        classes = get_user_classes(user)
+        teachers = get_teachers(user)
+        times = get_class_times(user)
+        enrolled = get_enrolled_students(user)
+        cap = get_class_capacity(user)
+        return render_template(
+            'user_page.html', classes=classes, teachers=teachers, times=times, enrolled=enrolled, cap=cap, name=name
+        )
+
+
+@app.route('/teacher', methods=['POST', 'GET'])
+def teacher_page():
+    user = Users.query.filter_by(id=current_user.id).first()
+    teacher = Teachers.query.filter(Teachers.user_id == user.id).first()
+    name = "Dr. " + teacher.last_name
     classes = get_user_classes(user)
-    teachers = get_teachers(user)
+    teachers = []
+    for entry in classes:
+        c = Classes.query.filter(Classes.class_name == entry).first()
+        teacher = Teachers.query.filter(c.teacher_id == Teachers.id).first()
+        teachers.append(teacher.first_name + " " + teacher.last_name)
     times = get_class_times(user)
     enrolled = get_enrolled_students(user)
     cap = get_class_capacity(user)
-    return render_template('user_page.html', classes=classes,teachers=teachers,times=times,enrolled=enrolled,cap=cap,name=name)
+    return render_template(
+        'user_page.html', classes=classes, enrolled=enrolled, times=times, name=name, cap=cap, teachers=teachers
+    )
 
 
-@app.route('/user_teacher/<user_id>', methods=['POST', 'GET'])
-def teacher_page(user_id):
-    user = Users.query.filter_by(id=user_id).first()
-    classes = get_user_classes(user)
-    enrolled = get_enrolled_students(user)
-    return render_template('user_page.html', classes=classes, enrolled=enrolled)
+@app.route('/user/registration', methods=['POST', 'GET'])
+@login_required
+def registration():
+    if request.method == 'POST':
+        return redirect(url_for('user'))
+    user = Users.query.filter_by(id=current_user.id).first()
+    student = Students.query.filter(Students.user_id == user.id).first()
+    name = student.first_name
+    classes = Classes.query.all()
+    class_names = []
+    times = []
+    enrolled = []
+    cap = []
+    for c in classes:
+        class_names.append(c.class_name)
+        times.append(c.timeslot)
+        enrolled.append(c.enrolled)
+        cap.append(c.size)
+    # full = check_class_capacity()
+    return render_template(
+        'registration.html', class_names=class_names, times=times, enrolled=enrolled, cap=cap,
+        name=name, classes=classes, teachers=Teachers
+    )
 
 
 @app.route('/logout')
